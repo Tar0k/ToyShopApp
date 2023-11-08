0c74d049af8ce5b41396152d2d1b2134e9423812
Tar0k <73.tarok@gmail.com>
2023-11-08 23:41:50 +0400

    Init

---
+++ ToyShopApp/Program.cs
@@ -0,0 +1,9 @@
+﻿var toyShop = new ToyShop.ToyShop();
+
+
+toyShop.Add("Робот", quantity:101, dropRate:101);
+toyShop.Add("Кукла", quantity:3, dropRate:30);
+toyShop.Add("Машинка", quantity:10, dropRate:10);
+toyShop.Add("Кукла", quantity:2);
+
+Console.WriteLine(toyShop);
---
+++ ToyShopApp/ToyShop.cs
@@ -0,0 +1,49 @@
+﻿namespace ToyShop;
+
+public class ToyShop
+{
+    private readonly List<ToyStack> _toyStacks = new();
+
+    public void Add(string name, int dropRate = 0, int quantity = 1)
+    {
+        var toyStack = _toyStacks.Find(x => x.Name == name);
+        if (toyStack != null)
+        {
+            toyStack += quantity;
+            if (dropRate != 0) toyStack.DropRate = dropRate;
+        }
+        else
+        {
+            var maxId = 0; 
+            if (_toyStacks.Count > 0)
+            {
+                maxId = _toyStacks.Max(x => x.Id);
+            }
+            _toyStacks.Add(new ToyStack(maxId + 1, name, dropRate, quantity));
+        }
+    }
+
+    public void DropRate(string name, int newDropRate)
+    {
+        var toyStack = _toyStacks.Find(x => x.Name == name);
+        if (toyStack != null)
+        {
+            toyStack.DropRate = newDropRate;
+        }
+        else
+        {
+            throw new ArgumentException($"Игрушка с именем {name} не найдена");
+        }
+        
+    }
+
+    private bool CheckTotalDropRate()
+    {
+        if (_toyStacks.Count == 0) return false;
+        var totalDropRate = _toyStacks.Sum(x => x.DropRate);
+        return totalDropRate <= 100;
+    }
+    
+    
+    public override string ToString() => string.Join("\n", _toyStacks);
+}
